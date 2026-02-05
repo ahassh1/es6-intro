9e69f19e031451cb6278605c90dd53140524ccfa
ahassh1 <ahasanhabibpolash420@gmail.com>
2026-02-05 12:04:25 +0600

    find operation

---
+++ find.js
@@ -0,0 +1,5 @@
+const marks = [34, 46, 23, 67, 89, 90, 12, 49];
+const evenMarks = marks.find((mark) => mark % 2 == 0);
+console.log(evenMarks);
+const findMarks = marks.find((mark) => mark > 40);
+console.log(findMarks);
